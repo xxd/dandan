36638b94a5dc00c7cf0cffd4c47e8d27cc8ba820
vvdpzz <vvdpzz@gmail.com>
2012-03-24 10:22:45 +0800

    Initial Commit

---
+++ Dandan/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dandan
+//
+//  Created by 陈 振宇 on 12-3-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dandan/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Dandan
+//
+//  Created by 陈 振宇 on 12-3-24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
