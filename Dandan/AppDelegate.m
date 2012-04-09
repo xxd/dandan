303b769126d2807457fb59881af96f3e1c022304
xxd <xxdxxd51@hotmail.com>
2012-04-09 22:11:44 +0800

    The colon!!!

--- Dandan/AppDelegate.m
+++ Dandan/AppDelegate.m
@@ -53,23 +53,31 @@
 
 -(void)initializeDatabase{    
     if (sqlite3_open([databasePath UTF8String], &database) == SQLITE_OK) {
-        NSArray *tableArray = [NSArray arrayWithObjects:@"list", @"categorie", nil];
+        NSArray *tableArray = [NSArray arrayWithObjects:@"List", @"categorie", nil];
         for ( NSString *table in tableArray ){
             NSLog( @"table name:%@", table);
-            SEL customSelector = NSSelectorFromString([NSString stringWithFormat:@"syncWith%@", table]);
+            SEL customSelector = NSSelectorFromString([NSString stringWithFormat:@"syncWith%@:", table]);
+            NSLog( @"SEL:%s", customSelector);
             NSString *sql_str = [NSString stringWithFormat:@"SELECT id FROM %@s ORDER BY id DESC LIMIT 1", table];
             const char *sql = (char *)[sql_str UTF8String];
             NSLog( @"SQL :%@", sql_str);
             sqlite3_stmt *statement;
-            
+            Sync * sync = [Sync alloc];
             if (sqlite3_prepare_v2(database, sql, -1, &statement, NULL) == SQLITE_OK) {
                 while (sqlite3_step(statement) == SQLITE_ROW) {
-                    NSInteger primaryKey = sqlite3_column_int(statement, 0);
+                    int primaryKey = sqlite3_column_int(statement, 0);
                     NSLog(@"primaryKey：%i",primaryKey);
-                    
-                    Sync *sync = [[Sync alloc] syncWithList:primaryKey];
-                    listArray = [sync copy];
-                }
+                    NSNumber *pk = [NSNumber numberWithInt:primaryKey];
+                    NSLog(@"pk：%@",pk);
+                    if ([[Sync alloc] respondsToSelector:customSelector]) {
+                        sync = [[Sync alloc] performSelector:customSelector withObject:0];
+                        //sync = [[Sync alloc] syncWithList:primaryKey];
+                        listArray = [sync copy];
+                    }
+                    else {
+                        NSLog(@"## Class does not respond to %s", customSelector);
+                    }
+                                    }
             }
             sqlite3_finalize(statement);
             NSLog(@"listArray包含：%@",listArray);
